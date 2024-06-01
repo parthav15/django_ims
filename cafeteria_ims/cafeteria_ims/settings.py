c0d79ac9b2a4d90fff24350da3a87441591246c1
parthav15 <parthavsabrwal@gmail.com>
2024-06-01 12:33:03 +0530

    Vercel configuration settings

--- cafeteria_ims/cafeteria_ims/settings.py
+++ cafeteria_ims/cafeteria_ims/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--fiket)k56znkjz@^&s)8=_rk_r$n-3srf*jb&5su^xu^$ittq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -127,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
